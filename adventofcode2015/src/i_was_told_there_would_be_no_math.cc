bac2d5d8576fdc791672052818b89587de1159fc
Shikanime Deva <deva.shikanime@protonmail.com>
2022-04-10 01:46:19 +0200

    Add Advent of Code 2015 Day 3

---
+++ adventofcode2015/src/i_was_told_there_would_be_no_math.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <numeric>
+#include <tuple>
+#include <vector>
+
+int main(int argc, char const *argv[]) {
+  std::string str;
+  std::vector<int> papers;
+  while (std::getline(std::cin, str)) {
+    int l, w, h;
+    sscanf(str.c_str(), "%dx%dx%d", &l, &w, &h);
+    auto lw = l * w;
+    auto wh = w * h;
+    auto hl = h * l;
+    auto box = 2 * (lw + wh + hl);
+    auto slack = std::min(lw, std::min(wh, hl));
+    papers.push_back(box + slack);
+  }
+  auto res = std::accumulate(papers.begin(), papers.end(), 0);
+  std::cout << res << std::endl;
+  return 0;
+}
