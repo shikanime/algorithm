2c93637f7bf81fe7562bfd4731e0f23ea155c7a3
Shikanime Deva <deva.shikanime@protonmail.com>
2022-04-13 20:38:45 +0200

    Refactor Nix shell

--- adventofcode2015/shell.nix
+++ adventofcode2015/shell.nix
@@ -2,12 +2,9 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.deno
     pkgs.cmake
     pkgs.clang
     pkgs.clang-tools
-    pkgs.elixir
     pkgs.openssl
-    (pkgs.python39.withPackages (ps: with ps; [ poetry ]))
   ];
 }
--- flake.nix
+++ flake.nix
@@ -4,8 +4,23 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
 
   outputs = { self, nixpkgs, home-manager, ... }: {
-    devShell = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix (system:
-      import ./shell.nix { pkgs = import nixpkgs { inherit system; }; }
-    );
+    devShells = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix
+      (system: {
+        adventOfCode2015 =
+          import ./adventofcode2015/shell.nix { pkgs = import nixpkgs { inherit system; }; };
+        hashcode2020 =
+          import ./hashcode2020/shell.nix
+            { pkgs = import nixpkgs { inherit system; }; };
+        hashcode2022 =
+          import ./hashcode2022/shell.nix
+            { pkgs = import nixpkgs { inherit system; }; };
+        crackTheCodeInterviewElixir =
+          import ./crackthecodeinterview/elixir/shell.nix
+            { pkgs = import nixpkgs { inherit system; }; };
+        crackTheCodeInterviewNode =
+          import ./crackthecodeinterview/node/shell.nix
+            { pkgs = import nixpkgs { inherit system; }; };
+
+      });
   };
 }
