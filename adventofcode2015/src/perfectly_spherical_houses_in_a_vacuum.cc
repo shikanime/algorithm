1daa1c0075de52314a92a6fea683ede28c512461
Shikanime Deva <deva.shikanime@protonmail.com>
2022-04-13 15:01:33 +0200

    Add day 3

---
+++ adventofcode2015/src/perfectly_spherical_houses_in_a_vacuum.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <set>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+int main(int argc, char const *argv[]) {
+  auto chars = std::vector<char>(std::istream_iterator<char>(std::cin),
+                                 std::istream_iterator<char>());
+  auto houses = std::list<std::pair<int, int>>{chars.size(), {0, 0}};
+  std::transform(
+      chars.begin(), chars.end(), std::front_inserter(houses),
+      [&houses](char c) -> std::pair<int, int> {
+        switch (c) {
+        case '^':
+          return {std::get<0>(houses.front()), std::get<1>(houses.front()) + 1};
+        case 'v':
+          return {std::get<0>(houses.front()), std::get<1>(houses.front()) - 1};
+        case '<':
+          return {std::get<0>(houses.front()) - 1, std::get<1>(houses.front())};
+        case '>':
+          return {std::get<0>(houses.front()) + 1, std::get<1>(houses.front())};
+        }
+        throw std::runtime_error("invalid character");
+      });
+  auto res = std::set<std::pair<int, int>>(houses.begin(), houses.end());
+  std::cout << res.size() << std::endl;
+  return 0;
+}
