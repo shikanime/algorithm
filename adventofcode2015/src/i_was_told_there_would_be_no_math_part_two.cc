e837bde94fd58a92591632ee439bd91e9056eb34
Shikanime Deva <deva.shikanime@protonmail.com>
2022-04-14 23:56:24 +0200

    Fix unhandled return warning

--- adventofcode2015/src/i_was_told_there_would_be_no_math_part_two.cc
+++ adventofcode2015/src/i_was_told_there_would_be_no_math_part_two.cc
@@ -11,7 +11,8 @@ int main(int argc, char const* argv[]) {
   std::vector<int> ribbon;
   while (std::getline(std::cin, str)) {
     int l, w, h;
-    sscanf(str.c_str(), "%dx%dx%d", &l, &w, &h);
+    if (sscanf(str.c_str(), "%dx%dx%d", &l, &w, &h) != 3)
+      throw std::runtime_error("invalid payload");
     auto sides = std::array<int, 3>{l, w, h};
     std::sort(sides.begin(), sides.end());
     auto box = 2 * sides[0] + 2 * sides[1];
