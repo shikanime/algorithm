1788b9f44aac2bff87a6895957f8eabebb99f824
Shikanime Deva <deva.shikanime@protonmail.com>
2022-04-18 01:41:52 +0200

    Partial refactor to ranges

--- adventofcode2015/src/doesn_t_he_have_intern_elves_for_this.cc
+++ adventofcode2015/src/doesn_t_he_have_intern_elves_for_this.cc
@@ -2,36 +2,36 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <ranges>
 #include <string>
 #include <vector>
 
-template <class InputIterator>
-auto has_enough_vowels(InputIterator first, InputIterator last) {
-  auto vowels = std::count_if(first, last, [](char c) {
+template <std::ranges::input_range R>
+auto has_enough_vowels(R r) {
+  auto vowels = std::ranges::count_if(r, [](char c) {
     return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
   });
   return vowels >= 3;
 }
 
-template <class InputIterator>
-auto has_pair(InputIterator first, InputIterator last) {
-  return std::any_of(first, last, [](std::pair<char, char>& pair) {
-    return pair.first == pair.second;
-  });
+template <std::ranges::input_range R>
+auto has_pair(R r) {
+  return std::ranges::any_of(
+      r, [](std::pair<char, char>& pair) { return pair.first == pair.second; });
 }
 
-template <class InputIterator>
-auto has_only_valid_chars(InputIterator first, InputIterator last) {
+template <std::ranges::input_range R>
+auto has_only_valid_chars(R r) {
   static const auto excluded = std::vector<std::pair<char, char>>{
       {'a', 'b'}, {'c', 'd'}, {'p', 'q'}, {'x', 'y'}};
-  return std::none_of(first, last, [](std::pair<char, char>& pair) {
-    auto x = std::find(std::begin(excluded), std::end(excluded), pair);
-    return x != std::end(excluded);
+  return std::ranges::none_of(r, [](std::pair<char, char>& pair) {
+    return std::ranges::find(std::views::all(excluded), pair) !=
+           std::end(excluded);
   });
 }
 
-template <class InputIterator, class OutputIterator>
-auto window(InputIterator first, InputIterator last, OutputIterator out) {
+template <class I, class O>
+auto window(I first, I last, O out) {
   std::transform(first, last - 1, first + 1, out,
                  [](char a, char b) -> std::pair<char, char> {
                    return {a, b};
@@ -42,11 +42,11 @@ int main(int argc, char const* argv[]) {
   unsigned int i = 0;
   std::string line;
   while (std::getline(std::cin, line))
-    if (has_enough_vowels(std::begin(line), std::end(line))) {
+    if (has_enough_vowels(std::views::all(line))) {
       std::vector<std::pair<char, char>> pairs;
       window(std::begin(line), std::end(line), std::back_inserter(pairs));
-      if (has_pair(std::begin(pairs), std::end(pairs)) &&
-          has_only_valid_chars(std::begin(pairs), std::end(pairs)))
+      if (has_pair(std::views::all(pairs)) &&
+          has_only_valid_chars(std::views::all(pairs)))
         i++;
     }
   std::cout << i << std::endl;
