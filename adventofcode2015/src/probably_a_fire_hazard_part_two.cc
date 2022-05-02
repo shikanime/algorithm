c8a4365a39aa9ab42e68f67cabd9be937880f482
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-05-02 18:27:31 +0200

    Add day 6 part two

---
+++ adventofcode2015/src/probably_a_fire_hazard_part_two.cc
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <ranges>
+#include <regex>
+#include <string>
+
+enum class action { turn_on, turn_off, toggle };
+
+auto read_action(std::string s) -> action {
+  if (s == "turn on")
+    return action::turn_on;
+  else if (s == "turn off")
+    return action::turn_off;
+  else if (s == "toggle")
+    return action::toggle;
+  else
+    throw std::runtime_error("unknown instruction");
+}
+
+auto cartesian_product_view(std::ranges::input_range auto a,
+                            std::ranges::input_range auto b) {
+  return a | std::views::transform([b](const auto x) {
+           return b | std::views::transform([x](const auto y) {
+                    return std::pair{x, y};
+                  });
+         }) |
+         std::views::join;
+}
+
+int main(int argc, char const* argv[]) {
+  std::array<std::array<int, 1000>, 1000> grid{{0}};
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    static const std::regex command_regex(
+        "(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)");
+    std::smatch smatch;
+    if (std::regex_search(line, smatch, command_regex)) {
+      auto ax = std::stoi(smatch[2]);
+      auto ay = std::stoi(smatch[4]);
+      auto bx = std::stoi(smatch[3]);
+      auto by = std::stoi(smatch[5]);
+      auto action = read_action(smatch[1]);
+      auto matrix = cartesian_product_view(std::views::iota(ax, ay + 1),
+                                           std::views::iota(bx, by + 1));
+      std::ranges::for_each(matrix, [&grid, &action](const auto xy) {
+        switch (action) {
+          case action::turn_on:
+            grid[xy.first][xy.second] += 1;
+            break;
+          case action::turn_off:
+            if (grid[xy.first][xy.second] > 0) grid[xy.first][xy.second] -= 1;
+            break;
+          case action::toggle:
+            grid[xy.first][xy.second] += 2;
+            break;
+        }
+      });
+    }
+  }
+  auto res = std::accumulate(std::cbegin(grid), std::cend(grid), 0,
+                             [](const auto acc, const auto& row) {
+                               return acc + std::accumulate(std::cbegin(row),
+                                                            std::cend(row), 0);
+                             });
+  std::cout << res << std::endl;
+  return 0;
+}
