e88fe92179a1e8ecc37c23bb1e63e647e5609957
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-23 17:25:45 +0000

    Add how sum with tabulation

---
+++ freecodecamp/dynamicprogramming/tabulation/how_sum.exs
@@ -0,0 +1,49 @@
+#!/usr/bin/env elixir
+
+import ExUnit.Assertions
+
+defmodule HowSum do
+  def solve(target, numbers) when is_number(target) do
+    solve([[] | List.duplicate(nil, target)], numbers)
+  end
+
+  def solve([], _numbers) do
+    []
+  end
+
+  def solve([nil | tail], numbers) do
+    [nil | solve(tail, numbers)]
+  end
+
+  def solve([combinaisons | tail], numbers) do
+    result =
+      tail
+      |> Enum.with_index(1)
+      |> Enum.map(&toggle(&1, numbers, combinaisons))
+
+    [combinaisons | solve(result, numbers)]
+  end
+
+  defp toggle({nil, idx}, numbers, combinaisons) do
+    if number = Enum.find(numbers, &(&1 == idx)) do
+      [number | combinaisons]
+    end
+  end
+
+  defp toggle({combinaison, _idx}, _numbers, _combinaisons) do
+    combinaison
+  end
+end
+
+assert(
+  [
+    [],
+    nil,
+    nil,
+    [3],
+    [4],
+    [5],
+    [3, 3],
+    [4, 3]
+  ] == HowSum.solve(7, [5, 3, 4])
+)
