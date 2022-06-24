de881f3e28473097d3770d8f5a7d46733980deca
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-24 10:15:57 +0000

    Add valid anagram

---
+++ leetcode/valid_anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if not len(s) == len(t):
+            return False
+        shs: dict[str, int] = {}
+        ths: dict[str, int] = {}
+        for cs, ct in zip(s, t):
+            csi = shs.get(cs) or 0
+            shs[cs] = csi + 1
+            tsi = ths.get(ct) or 0
+            ths[ct] = tsi + 1
+        return shs == ths
+
+
+solver = Solution()
+assert solver.isAnagram("anagram", "nagaram") == True
