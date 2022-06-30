000dcf13f32575b1b12f49f9419d7a6a41a8ff7d
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-30 11:21:34 +0000

    Add same tree in iterative

--- leetcode/same_tree.cc
+++ leetcode/same_tree.cc
@@ -1,8 +1,18 @@
 #include "same_tree.h"
 
+#include <queue>
+
 bool Solution::isSameTree(TreeNode* p, TreeNode* q) {
-  if (p == nullptr && q == nullptr) return true;
-  if (p == nullptr || q == nullptr) return false;
-  if (p->val != q->val) return false;
-  return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  auto queue = std::queue<std::pair<TreeNode*, TreeNode*>>();
+  queue.emplace(p, q);
+  while (!queue.empty()) {
+    auto [p, q] = queue.front();
+    queue.pop();
+    if (p == nullptr && q == nullptr) continue;
+    if (p == nullptr || q == nullptr) return false;
+    if (p->val != q->val) return false;
+    queue.emplace(p->left, q->left);
+    queue.emplace(p->right, q->right);
+  }
+  return true;
 }
