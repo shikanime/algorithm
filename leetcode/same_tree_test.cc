5c9594c56ace72127bc182a839ea420fa382fa2c
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-30 13:09:07 +0000

    Add subtree search

--- leetcode/same_tree_test.cc
+++ leetcode/same_tree_test.cc
@@ -2,9 +2,13 @@
 
 #include <gtest/gtest.h>
 
-TEST(SameTree, SolutionAssertions) {
+TEST(SameTree, Base) {
   auto solution = Solution();
   EXPECT_EQ(true, solution.isSameTree(nullptr, nullptr));
+}
+
+TEST(SameTree, Normal) {
+  auto solution = Solution();
   auto left = TreeNode(0);
   EXPECT_EQ(false, solution.isSameTree(&left, nullptr));
 }
---
+++ leetcode/subtree_of_another_tree_test.cc
@@ -0,0 +1,41 @@
+#include "subtree_of_another_tree.h"
+
+#include <gtest/gtest.h>
+
+TEST(SubTree, Base) {
+  auto solution = Solution();
+  EXPECT_EQ(true, solution.isSubtree(nullptr, nullptr));
+}
+
+TEST(SubTree, Normal) {
+  auto solution = Solution();
+
+  auto lll = TreeNode(1);
+  auto llr = TreeNode(2);
+  auto ll = TreeNode(4, &lll, &llr);
+  auto lr = TreeNode(5);
+  auto l = TreeNode(3, &ll, &lr);
+
+  auto rl = TreeNode(1);
+  auto rr = TreeNode(2);
+  auto r = TreeNode(4, &rl, &rr);
+
+  EXPECT_EQ(true, solution.isSubtree(&l, &r));
+}
+
+TEST(SubTree, NonMediumTreeSubTree) {
+  auto solution = Solution();
+
+  auto lllr = TreeNode(0);
+  auto llr = TreeNode(2, nullptr, &lllr);
+  auto lll = TreeNode(1);
+  auto lr = TreeNode(5);
+  auto ll = TreeNode(4,&lll, &llr);
+  auto l = TreeNode(3, &ll, &lr);
+
+  auto rl = TreeNode(1);
+  auto rr = TreeNode(2);
+  auto r = TreeNode(4, &rl, &rr);
+
+  EXPECT_EQ(false, solution.isSubtree(&l, &r));
+}
