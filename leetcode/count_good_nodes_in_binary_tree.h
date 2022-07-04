e38ea46c6cbcd52080664a03aade8c07b92b05e3
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-07-04 14:30:08 +0000

    Add good nodes in bin tree

---
+++ leetcode/count_good_nodes_in_binary_tree.h
@@ -0,0 +1,32 @@
+#include <utility>
+#include <algorithm>
+
+namespace {
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  int goodNodes(TreeNode* root) {
+    if (root == nullptr) return 0;
+    return traverse(root, root->val);
+  }
+
+ private:
+  int traverse(TreeNode* root, int limit) {
+    if (root == nullptr) return 0;
+    auto curr = std::max(limit, root->val);
+    auto res = root->val >= limit ? 1 : 0;
+    res += traverse(root->left, curr);
+    res += traverse(root->right, curr);
+    return res;
+  }
+};
+}  // namespace
---
+++ leetcode/count_good_nodes_in_binary_tree_test.cc
@@ -0,0 +1,43 @@
+#include "count_good_nodes_in_binary_tree.h"
+
+#include <gtest/gtest.h>
+
+TEST(CountGoodNodesInBinaryTree, Base) {
+  auto solution = Solution();
+  EXPECT_EQ(solution.goodNodes(nullptr), 0);
+}
+
+TEST(CountGoodNodesInBinaryTree, SingleEntry) {
+  auto solution = Solution();
+  auto input = TreeNode(1);
+  EXPECT_EQ(solution.goodNodes(&input), 1);
+}
+
+TEST(CountGoodNodesInBinaryTree, MultiLevelWithMiddleLowerTree) {
+  auto solution = Solution();
+  auto ilr = TreeNode(4);
+  auto ir = TreeNode(1);
+  auto il = TreeNode(2, &ilr, nullptr);
+  auto input = TreeNode(3, &ir, &il);
+  EXPECT_EQ(solution.goodNodes(&input), 2);
+}
+
+TEST(CountGoodNodesInBinaryTree, BadMiddleTree) {
+  auto solution = Solution();
+  auto ilr = TreeNode(5);
+  auto ill = TreeNode(1);
+  auto irl = TreeNode(3);
+  auto ir = TreeNode(1, &irl, nullptr);
+  auto il = TreeNode(4, &ilr, &ill);
+  auto input = TreeNode(3, &ir, &il);
+  EXPECT_EQ(solution.goodNodes(&input), 4);
+}
+
+TEST(CountGoodNodesInBinaryTree, SingleBadEnded) {
+  auto solution = Solution();
+  auto irr = TreeNode(2);
+  auto irl = TreeNode(4);
+  auto ir = TreeNode(3, &irl, &irr);
+  auto input = TreeNode(3, &ir, nullptr);
+  EXPECT_EQ(solution.goodNodes(&input), 3);
+}
