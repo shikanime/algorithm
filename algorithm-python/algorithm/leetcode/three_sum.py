951c6fc4f930095f6e6129d067a823ee488c8e0a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-17 00:08:13 +0200

    Optimize three sum

--- algorithm-python/algorithm/leetcode/three_sum.py
+++ algorithm-python/algorithm/leetcode/three_sum.py
@@ -1,12 +1,15 @@
 class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        res: list[list[int]] = []
-        memo: set[int] = set()
-        for n in nums:
-            for m in memo:
-                if m < n:
-                    s = m - n
-                    if s in memo:
-                        res.append([n, m, s])
-            memo.add(n)
+    def threeSum(self, nums: list[int]) -> set[tuple[int, int, int]]:
+        res: set[tuple[int, int, int]] = set()
+        for i, target in enumerate(nums):
+            # Fastlane skip repeated occurrence
+            if i > 0 and target == nums[i - 1]:
+                continue
+            subnums = nums[:i] + nums[i + 1 :]
+            memo: dict[int, int] = {}
+            for i, n in enumerate(subnums):
+                diff = -target - n
+                if diff in memo:
+                    res.add(tuple(sorted([target, subnums[memo[diff]], subnums[i]])))
+                memo[n] = i
         return res
--- algorithm-python/algorithm/leetcode/three_sum_test.py
+++ algorithm-python/algorithm/leetcode/three_sum_test.py
@@ -3,4 +3,5 @@ from .three_sum import Solution
 
 def test_solution():
     solver = Solution()
-    assert solver.threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+    assert solver.threeSum([-1, 0, 1, 2, -1, -4]) == set([(-1, 0, 1), (-1, -1, 2)])
+    assert solver.threeSum([0, 0, 0, 0]) == set([(0, 0, 0)])
