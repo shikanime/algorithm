d875fc2fb37f9821136117beb2e18d2fd068e297
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-16 16:26:36 +0200

    Add Python challenge generator

---
+++ algorithm-python/algorithm/scripts/generate.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import jinja2
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, required=True)
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--template", type=str, required=True)
+    args, _ = parser.parse_known_args()
+
+    loader = jinja2.PackageLoader("algorithm", f"templates/{args.template}")
+    env = jinja2.Environment(
+        loader=loader, extensions=["jinja2_strcase.StrcaseExtension"]
+    )
+
+    path = Path(args.path)
+    path.mkdir(parents=True, exist_ok=True)
+
+    test_file = path / f"{args.name}_test.py"
+    with test_file.open("w", encoding="utf-8") as f:
+        test_template = env.get_template("test.py.jinja2")
+        f.write(test_template.render(name=args.name))
+
+    source_file = path / f"{args.name}.py"
+    with source_file.open("w", encoding="utf-8") as f:
+        source_template = env.get_template("source.py.jinja2")
+        f.write(source_template.render(name=args.name))
+
+
+if __name__ == "__main__":
+    main()
