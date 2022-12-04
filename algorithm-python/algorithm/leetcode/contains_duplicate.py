5946e7ffbf37f29a2f6788d663f348e36d242f41
Shikanime Deva <william.phetsinorath@shikanime.studio>
2022-12-04 21:50:03 +0000

    Add contains duplicate

--- algorithm-python/algorithm/leetcode/contains_duplicate.py
+++ algorithm-python/algorithm/leetcode/contains_duplicate.py
@@ -1,8 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        hs: set[int] = set()
-        for num in nums:
-            if num in hs:
-                return False
-            hs.add(num)
-        return True
+        return len(set(nums)) != len(nums)
