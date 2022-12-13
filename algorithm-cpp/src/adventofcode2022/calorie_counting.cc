2bc60ca10b44586a94ca39daad8795cbffb4318c
Shikanime Deva <william.phetsinorath@shikanime.studio>
2022-12-13 20:34:42 +0000

    Fix advent of code 2022

--- algorithm-cpp/src/adventofcode2022/calorie_counting.cc
+++ algorithm-cpp/src/adventofcode2022/calorie_counting.cc
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
-#include <numeric>
+#include <algorithm>
 
 int solve(std::istream& is) {
-  std::vector<int> calories;
-  std::for_each(std::istream_iterator<std::string>{std::cin},
-                std::istream_iterator<std::string>{},
-                [&calories](std::string c) {
-                  if (c == "") {
-                    calories.push_back(0);
-                  } else {
-                    calories.back() += std::atoi(c.c_str());
-                  }
-                });
-  return *std::max_element(calories.begin(), calories.end());
+  std::vector<int> calories{0};
+  std::string line;
+  while (std::getline(is, line)) {
+    if (line == "") {
+      calories.push_back(0);
+    } else {
+      calories.back() += std::stoi(line);
+    }
+  }
+  return *std::ranges::max_element(calories.begin(), calories.end());
 }
 
 int main(int argc, char const* argv[]) {
