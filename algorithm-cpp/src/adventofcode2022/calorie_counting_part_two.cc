62d57ace87e6edf68db46c4a5dfc08e0bcf44231
Shikanime Deva <william.phetsinorath@shikanime.studio>
2022-12-13 23:57:39 +0000

    Add advent of code day 1 part 2

---
+++ algorithm-cpp/src/adventofcode2022/calorie_counting_part_two.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int solve(std::istream& is, int k) {
+  std::vector<int> calories{0};
+  std::string line;
+  while (std::getline(is, line)) {
+    if (line == "") {
+      calories.push_back(0);
+    } else {
+      calories.back() += std::stoi(line);
+    }
+  }
+  std::vector<int> top;
+  for (auto i = 0; k > i; i++) {
+    auto elem = std::ranges::max_element(calories.begin(), calories.end());
+    top.emplace_back(*elem);
+    calories.erase(elem);
+  }
+  return std::accumulate(top.begin(), top.end(), 0);
+}
+
+int main(int argc, char const* argv[]) {
+  std::cout << solve(std::cin, 3) << std::endl;
+  return 0;
+}
