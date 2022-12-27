463b0523b2aee5872dd4f66ea68a523326bf6cf9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-12-27 15:57:01 +0100

    Refactor top k counter

--- algorithm-python/algorithm/leetcode/top_k_frequent_elements.py
+++ algorithm-python/algorithm/leetcode/top_k_frequent_elements.py
@@ -1,11 +1,9 @@
-import collections
+from collections import Counter
 
 
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        count: dict[int, int] = collections.defaultdict(int)
-        for num in nums:
-            count[num] += 1
+        count = Counter(nums)
         top: list[list[int]] = [[] for _ in range(0, len(nums))]
         for ck, cc in count.items():
             top[cc - 1].append(ck)
