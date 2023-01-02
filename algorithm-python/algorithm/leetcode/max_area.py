be11df641ecc43c2c437f18b2eb2e917f3711957
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-01-02 21:26:30 +0100

    Simplify max area

--- algorithm-python/algorithm/leetcode/max_area.py
+++ algorithm-python/algorithm/leetcode/max_area.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        best_left, best_right = left, right = 0, len(height) - 1
+        best, left, right = 0, 0, len(height) - 1
         while left < right:
-            if self.area(height, left, right) > self.area(
-                height, best_left, best_right
-            ):
-                best_left, best_right = left, right
+            area = min(height[right], height[left]) * (right - left)
             if height[left] > height[right]:
                 right -= 1
             else:
                 left += 1
-
-        return self.area(height, best_left, best_right)
-
-    def area(self, height: list[int], left: int, right: int):
-        h = min(height[left], height[right])
-        w = right - left
-        return h * w
+            best = max(best, area)
+        return best
