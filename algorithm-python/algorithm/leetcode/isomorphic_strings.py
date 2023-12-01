b0324a23f3598d04feea3b19a345de24807a6b34
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-12-01 09:48:23 +0000

    Refactor VSCode config to workspace

--- algorithm-python/algorithm/leetcode/isomorphic_strings.py
+++ algorithm-python/algorithm/leetcode/isomorphic_strings.py
@@ -2,7 +2,7 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_vocab = list(dict.fromkeys(s))
         t_vocab = list(dict.fromkeys(t))
-        for (cs, ct) in zip(s, t):
+        for cs, ct in zip(s, t):
             if s_vocab.index(cs) != t_vocab.index(ct):
                 return False
         return True
