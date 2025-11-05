9b1865486c6352cbb27e3fde5d26239ca7b08a91
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-05 22:32:23 +0100

    Migrate nix run to flake
    [ghstack-poisoned]

--- nix/flake/base.nix
+++ nix/flake/base.nix
@@ -1,27 +1,35 @@
 { inputs, ... }:
 
 {
-  perSystem = _: {
-    treefmt = {
-      enableDefaultExcludes = true;
-      projectRootFile = "flake.nix";
-      programs = {
-        nixfmt.enable = true;
-        prettier.enable = true;
-        shfmt.enable = true;
-        statix.enable = true;
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        enableDefaultExcludes = true;
+        projectRootFile = "flake.nix";
+        programs = {
+          nixfmt.enable = true;
+          prettier.enable = true;
+          shfmt.enable = true;
+          statix.enable = true;
+        };
+        settings.global.excludes = [
+          "LICENSE"
+        ];
       };
-      settings.global.excludes = [
-        "LICENSE"
-      ];
+      devenv = {
+        modules = [
+          inputs.devlib.devenvModule
+        ];
+        shells = {
+          default.imports = [
+            ../devenv/base.nix
+          ];
+          update.packages = [
+            pkgs.nushell
+            pkgs.sapling
+          ];
+        };
+      };
     };
-    devenv = {
-      modules = [
-        inputs.devlib.devenvModule
-      ];
-      shells.default.imports = [
-        ../devenv/base.nix
-      ];
-    };
-  };
 }
