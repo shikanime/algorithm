3b34ae7619d4677ca8606261740e0e6411c0278f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-09 10:56:51 +0100

    Add checks
    [ghstack-poisoned]

--- nix/flake/cc.nix
+++ nix/flake/cc.nix
@@ -1,17 +1,32 @@
 {
-  perSystem = _: {
-    treefmt = {
-      programs = {
-        clang-format.enable = true;
-        cmake-format.enable = true;
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        programs = {
+          clang-format.enable = true;
+          cmake-format.enable = true;
+        };
+        settings.global.excludes = [
+          "algorithm-cc/share/*"
+        ];
       };
-      settings.global.excludes = [
-        "algorithm-cc/share/*"
+      checks.cc =
+        pkgs.runCommandLocal "cc"
+          { src = ../../algorithm-cc; }
+          ''
+            ${pkgs.cmake}/bin/cmake \
+              --preset unknown-unknown-gnu \
+              -B out/build/unknown-unknown-gnu
+            ${pkgs.cmake}/bin/cmake \
+              --build out/build/unknown-unknown-gnu
+            ${pkgs.cmake}/bin/ctest \
+              --preset unknown-unknown-gnu \
+              --test-dir out/build/unknown-unknown-gnu
+          '';
+      devenv.shells.cc.imports = [
+        ../devenv/base.nix
+        ../devenv/cc.nix
       ];
     };
-    devenv.shells.cc.imports = [
-      ../devenv/base.nix
-      ../devenv/cc.nix
-    ];
-  };
 }
